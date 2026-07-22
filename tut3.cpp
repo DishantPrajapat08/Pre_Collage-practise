7f98f5f58176f8fff7a6a08e5a5670edd72b4d67
DishantPrajapat08 <dishantprajapat08>
2026-07-22 13:30:45 +0530

    First Long operation

---
+++ tut3.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=10,b=20,c=30;
+
+    // Multiple operations in one line
+
+    cout<<"The operation is "<<endl;
+    cout<<a++ + b<<endl;
+    cout<<c;
+    
+
+
+}
