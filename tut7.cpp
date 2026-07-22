e3d407bb2ddd2d45b96bf7b5fe9f4b2462feae7a
DishantPrajapat08 <dishantprajapat08>
2026-07-22 23:36:35 +0530

    7 table

--- tut7.cpp
+++ tut7.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 int main(){
 
-    int age;
-    cout<<"Enter your age"<<endl;
-    cin>>age;
+    // int age;
+    // cout<<"Enter your age"<<endl;
+    // cin>>age;
 
-    while (age<=17){
-        cout<<"Not elligable for team";
-        break;
-    }
-    while(age>=18){
-        cout<<"eligible for team";
-        break;
-    }
+    // while (age<=17){
+    //     cout<<"Not elligable for team";
+    //     break;
+    // }
+    // while(age>=18){
+    //     cout<<"eligible for team";
+    //     break;
+    // }
 
+
+   for(int a=7;a<=100;a++)
+
+ if(a%7==0){
+cout<<a<<endl;
+ }
+
+ 
     return 0;
 }
 
