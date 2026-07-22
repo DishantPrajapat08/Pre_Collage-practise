a88a5e542c93946ec33c9bad81433a7e57ebfce0
DishantPrajapat08 <dishantprajapat08>
2026-07-22 12:42:45 +0530

    Mathematical Operations

---
+++ tut2.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    int a=12,b=80;
+
+    // Mathematical operations
+
+    cout<< "The sum of a and b is"<<endl;
+    cout<< a+b;
+
+    
+    return 0;
+
+
+}
+    
+    
+
+    
