385456f1e433eee0acf0747f9d4fa9614f289194
DishantPrajapat08 <dishantprajapat08>
2026-07-22 22:57:51 +0530

    Line No

--- tut7.cpp
+++ tut7.cpp
@@ -1,11 +1,11 @@
-//fOR LOOP WHILE LOOP AND DO WHILE LOOP
+//FOR LOOP WHILE LOOP AND DO WHILE LOOP
 
 # include <iostream>
 using namespace std;
 
 int main(){
-
-    
+for(int a=10;((a>=9)&&(a<=100));a++)
+   cout<<a++<<endl;
     return 0;
 }
 
