ab9d92dadb54b94f82e4c707e60f560626652b56
DishantPrajapat08 <dishantprajapat08>
2026-07-22 20:41:00 +0530

    corr

---
+++ tut6.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+// Switch Case
+ int age;
+
+cout<<"Enter your age"<<endl;
+cin>>age;
+
+switch (age)
+{
+case 18 :
+    {
+        cout<<" Aajan party me";
+    }
+    break;
+
+case 19:
+{
+    cout<<"Nahi aana party me";
+}    
+break;
+
+default :
+{
+    cout<<"Kon hai bhai";
+}
+break;
+
+}
+    return 0;
+
+}
---
+++ tut7.cpp
@@ -0,0 +1,13 @@
+//HW table of 1305
+# include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int n=1305;
+for(int m=1305;m<=200000;m++)
+    cout<<m<<endl;
+
+    return 0;
+}
