9245eb7f1d0c6660f5e5fbfe66d4a3a9935d85ec
DishantPrajapat08 <dishantprajapat08>
2026-07-22 12:55:57 +0530

    More Calculations

--- tut2.cpp
+++ tut2.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 int main()
 {
-    int a=12,b=80;
+    int a=12,b=62;
 
     // Mathematical operations
 
+
     cout<< "The sum of a and b is"<<endl;
-    cout<< a+b;
+    cout<< a+b<<endl;
+
+    cout<< "The Product of a and b is"<<endl;
+    cout<< a*b<<endl;
+
+    cout<< "The Difference of a and b is"<<endl;
+    cout<< a-b<<endl;
+
+    cout<< "The Divison of a and b is"<<endl;
+    cout<< b/a<<endl;
+
+    cout<< "The Operation of a and b is"<<endl;
+    cout<< b%a<<endl;
+
+    
+
 
     
     return 0;
