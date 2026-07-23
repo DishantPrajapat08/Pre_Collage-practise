81c861c37c0a8be25d0dfd0c4bb87011860bc307
DishantPrajapat08 <dishantprajapat08>
2026-07-23 09:59:32 +0530

    Pointer

---
+++ tut8.cpp
@@ -0,0 +1,27 @@
+// Pointers,arrays
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    int*b=&a;
+
+    cout<<"The value is"<<endl;
+   cout<<a<<endl;
+   
+    cout<<"The value is"<<endl;
+   cout<<*b<<endl;
+
+    cout<<"The value is"<<endl;
+   cout<<b<<endl;
+
+    cout<<"The value is"<<endl;
+   cout<<&b<<endl;
+
+    cout<<"The value is"<<endl;
+   cout<<&a<<endl;
+
+  
+    return 0;
+}
