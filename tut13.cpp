cf07363d559dd5887ef3a5ebb3e7fbab46341299
DishantPrajapat08 <dishantprajapat08>
2026-07-23 14:23:57 +0530

    enum

---
+++ tut13.cpp
@@ -0,0 +1,13 @@
+//enum
+# include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+    enum meal{breakfast,lunch,dinner};
+    cout<<breakfast<<endl;
+    cout<<lunch<<endl;
+    return 0;
+}
