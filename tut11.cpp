c72c877e58671ac8ab72349f79538b4a4d400b08
DishantPrajapat08 <dishantprajapat08>
2026-07-23 13:57:02 +0530

    succesful union

--- tut11.cpp
+++ tut11.cpp
@@ -2,7 +2,24 @@
 # include <iostream>
 using namespace std;
 
+struct player
+{
+    int srno;
+    char favcar;
+};
+
+union country
+{
+int rollno;
+char car;
+};
+
 int main(){
-   
+
+    player dishant;
+    union country c1;
+    c1.rollno=7;
+    
+    cout<<c1.rollno;
     return 0;
 }
