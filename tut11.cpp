189b1dcf3ac68f0f04b918f8fae079e2ec7d5dac
DishantPrajapat08 <dishantprajapat08>
2026-07-23 13:33:47 +0530

    startofunion

--- tut11.cpp
+++ tut11.cpp
@@ -3,14 +3,6 @@
 using namespace std;
 
 int main(){
-    union country;{
-    int USA;
-    char INDIA;
-    float CHINA;
-    };
-    
-    union country c1;
-    c1.INDIA=10;
-
+   
     return 0;
 }
