2c97eb84404dc6c773dd48a5d20cc57ec2d0850c
DishantPrajapat08 <dishantprajapat08>
2026-07-23 21:48:08 +0530

    sum of n numbers

---
+++ tut15.cpp
@@ -0,0 +1,17 @@
+//sum of n numbers
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=10;
+    int sum=0;
+
+
+    for(int i=1;i<=n;i++){
+      sum=sum+i;
+    }
+
+    cout<<sum;
+    return 0;
+}
